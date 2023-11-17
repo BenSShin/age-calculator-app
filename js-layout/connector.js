9e4b2e449ad5ac7b141fa3ec1917e964fdfc8c3d
bshin27 <125755440+bshin27@users.noreply.github.com>
2023-11-17 16:02:56 -0600

    current date set as variables

--- js-layout/connector.js
+++ js-layout/connector.js
@@ -8,5 +8,20 @@ document.addEventListener("DOMContentLoaded", () => {
   // button click event
   button.addEventListener("click", function () {
     console.log("test");
+    // getting data from input
+    let day = inputDay.value;
+    let month = inputMonth.value;
+    let year = inputYear.value;
+
+    //getting current dates
+    let date = new Date();
+    let currentYear = date.getFullYear();
+    let currentMonth = date.getMonth() + 1;
+    let currentDay = date.getDate();
+    console.log(date);
+    console.log(currentYear, currentMonth, currentDay);
+
+    // displaying result in result fields
+    const outputDay = document.querySelector("#output_days").querySelector("p");
   });
 });
